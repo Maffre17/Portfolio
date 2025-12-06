fecfbe005d373b3566f541531b66d77d6cb3811e
Maffre17 <marcelafariasmff17@gmail.com>
2025-12-06 11:12:30 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,20 @@ document.querySelectorAll('.header__button-cta').forEach(btn => {
     };
 });
 
-const menu = document.querySelector('.header__menu-hamburger')
-const nav = document.querySelector('.header__nav')
-menu.addEventListener('click', () => {
-    if (nav.style.display === 'flex') {
-        nav.style.display = 'none'
-    } else {
-        nav.style.display = 'flex'
+const menu = document.querySelector('.header__menu-hamburger');
+const nav = document.querySelector('.header__nav');
+
+menu.addEventListener('click', (e) => {
+    e.stopPropagation();
+    nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex';
+});
+
+document.addEventListener('click', (e) => {
+    const clickForaDoNav = !nav.contains(e.target);
+    const clickNoMenu = menu.contains(e.target);
+
+    if (clickForaDoNav && !clickNoMenu) {
+        nav.style.display = 'none';
     }
-})
+});
+
